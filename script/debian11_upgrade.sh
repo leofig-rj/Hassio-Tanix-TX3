181711f60c7c77f48f39d7cb42de91bbbda5e047
Leonardo Figueiro <leoagfig@gmail.com>
2022-04-05 16:27:40 -0300

    Create debian11_upgrade.sh

---
+++ script/debian11_upgrade.sh
@@ -0,0 +1,83 @@
+#######################################################################
+#######################################################################
+##                                                                   ##
+## THIS SCRIPT SHOULD ONLY BE RUN ON A TANIX TX3 BOX RUNNING ARMBIAN ##
+##                                                                   ##
+#######################################################################
+#######################################################################
+set -o errexit  # Exit script when a command exits with non-zero status
+set -o errtrace # Exit on error inside any functions or sub-shells
+set -o nounset  # Exit script on use of an undefined variable
+set -o pipefail # Return exit status of the last command in the pipe that failed
+
+# ==============================================================================
+# SCRIPT LOGIC
+# ==============================================================================
+
+# ------------------------------------------------------------------------------
+# Installs all required software packages and tools
+# ------------------------------------------------------------------------------
+install_requirements() {
+  echo ""
+  echo "Updating APT packages list and upgrading..."
+  echo ""
+  sudo apt update
+  sudo apt upgrade -y
+  echo ""
+  echo "Ensure all requirements are installed..."
+  echo ""
+  sudo apt install gcc-8-base
+}
+
+# ------------------------------------------------------------------------------
+# Replace Debian 10 Repositories With Debian 11 Ones
+# ------------------------------------------------------------------------------
+config_sources_list() {
+  sudo rm /etc/apt/sources.list
+  {
+    echo "deb http://deb.debian.org/debian bullseye main contrib non-free";
+    echo "deb http://deb.debian.org/debian bullseye-updates main contrib non-free";
+    echo "deb http://security.debian.org/debian-security bullseye-security main";
+    echo "deb http://ftp.debian.org/debian bullseye-backports main contrib non-free";
+  } >> "/etc/apt/sources.list"
+}
+
+# ------------------------------------------------------------------------------
+# Upgrade final
+# ------------------------------------------------------------------------------
+upgrade_final() {
+  echo ""
+  echo "Upgrade to Debian 11..."
+  echo ""
+  sudo apt update
+  sudo apt full-upgrade
+  
+}
+
+# ==============================================================================
+# RUN LOGIC
+# ------------------------------------------------------------------------------
+main() {
+  # Are we root?
+  if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root."
+    echo "Please try again after running:"
+    echo "  sudo su"
+    exit 1
+  fi
+
+  # Install ALL THE THINGS!
+  install_requirements
+  config_sources_list
+  upgrade_final
+
+  # Friendly closing message
+  echo "======================================================================="
+  echo "Debian 11 installed."
+  echo "Verify the currently installed Debian version by reviewing below:"
+  
+  cat /etc/os-release
+  
+  exit 0
+}
+main
